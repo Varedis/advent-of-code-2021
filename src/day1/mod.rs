92e2e3acf8fe7e7c5657052837957fd6f9ff1411
Rob Scott <rob.scott@boots.co.uk>
2021-12-02 11:11:12 +0000

    day 1 formatting

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -30,7 +30,6 @@ pub fn exercise2(values: &[u32]) -> u32 {
 
 #[cfg(test)]
 mod tests {
-  // Note this useful idiom: importing names from outer (for mod tests) scope.
   use super::*;
 
   #[test]
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ fn main() {
     println!("Advent of Code 2021!");
     println!("--------------------");
 
-    let lines = helpers::read_file::lines_from_file("src/day1/day-1-input.txt");
+    let input = helpers::read_file::lines_from_file("src/day1/input.txt");
+
+    println!("d1 e1: {}", day1::exercise1(&input));
+    println!("d1 e2: {}", day1::exercise2(&input));
 
     println!("d1 e1: {}", day1::exercise1(&lines));
     println!("d1 e2: {}", day1::exercise2(&lines));
